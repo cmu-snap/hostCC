a7cb056b04bd5ea49a74734244da773e05ec8a45
Christopher Canel <c.canel@icloud.com>
2024-05-22 15:35:07 +0000

    Debugging CPU util

--- utils/cpu_util.py
+++ utils/cpu_util.py
@@ -1,31 +1,31 @@
+import collections
 import sys
 
 INPUT_FILE = sys.argv[1]
 
-cpu_util = {}
-num_samples = {}
+cpu_util = collections.defaultdict(float)
+num_samples = collections.defaultdict(float)
 
 with open(INPUT_FILE) as f1:
     for line in f1:
-        elements = line.split()
-        if len(elements) == 9 and elements[2] != "CPU":
-            cpu = int(elements[2])
-            util = float(elements[8])
-            if cpu not in cpu_util:
-                cpu_util[cpu] = (100 - util)
-                num_samples[cpu] = 1
-            else:
-                cpu_util[cpu] += (100 - util)
-                num_samples[cpu] += 1
+        if "CPU" not in line:
+            elements = line.split()
+            assert len(elements) == 8
+            cpu = int(elements[1])
+            idle = float(elements[7])
+            cpu_util[cpu] += 100 - idle
+            num_samples[cpu] += 1
 
 total_util = 0
 num_cpus = 0
 for cpu in cpu_util:
     if num_samples[cpu] != 0:
+        # Calculate average util for this CPU
         cpu_util[cpu] /= num_samples[cpu]
+        # Sum up util for all CPUs
         total_util += cpu_util[cpu]
         num_cpus += 1
 
-print("cpu_utils: ",cpu_util)
-print("num_samples: ",num_samples)
-print("avg_cpu_util: ", "" if num_cpus == 0 else total_util/num_cpus)
+print("cpu_utils:", cpu_util)
+print("num_samples:", num_samples)
+print("avg_cpu_util:", "" if num_cpus == 0 else total_util / num_cpus)
